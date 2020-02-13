a9604134572308d5639366860c9c53682e20c2e2
Vidy Videni <vidy.videni@gmail.com>
2020-02-13 22:20:29 +0800

    Can change object filter template

--- src/MuiForm/MuiForm.tsx
+++ src/MuiForm/MuiForm.tsx
@@ -1,7 +1,25 @@
-import { withTheme } from "react-jsonschema-form";
+import React from 'react';
+import { withTheme } from 'react-jsonschema-form';
+import ObjectFieldTemplateContext from '../ObjectFieldTemplate/ObjectFieldTemplateContext';
+import Theme from '../Theme';
+import { FormProps, ObjectFieldTemplateProps } from 'react-jsonschema-form';
 
-import Theme from "../Theme";
+const Form = withTheme(Theme);
 
-const MuiForm = withTheme(Theme);
+export interface MuiFormProps extends FormProps<any> {
+  objectFieldTemplates?: {
+    [key: string]: ObjectFieldTemplateProps;
+  };
+}
+
+const MuiForm = React.forwardRef((props: MuiFormProps, ref) => {
+  const { objectFieldTemplates = {}, ...rest } = props;
+
+  return (
+    <ObjectFieldTemplateContext.Provider value={objectFieldTemplates}>
+      <Form {...rest} ref={ref} />
+    </ObjectFieldTemplateContext.Provider>
+  );
+});
 
 export default MuiForm;
--- src/ObjectFieldTemplate/ObjectFieldTemplate.tsx
+++ src/ObjectFieldTemplate/ObjectFieldTemplate.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/styles';
-
+import ObjectFieldTemplateContext from './ObjectFieldTemplateContext';
 import { ObjectFieldTemplateProps } from 'react-jsonschema-form';
+import { getUiOptions } from 'react-jsonschema-form/lib/utils';
 
 const useStyles = makeStyles({
   root: {
@@ -11,46 +12,67 @@ const useStyles = makeStyles({
   },
 });
 
-const ObjectFieldTemplate = ({
-  DescriptionField,
-  description,
-  TitleField,
-  title,
-  properties,
-  required,
-  uiSchema,
-  idSchema,
-}: ObjectFieldTemplateProps) => {
+const ObjectFieldTemplate = (props: ObjectFieldTemplateProps) => {
+  const {
+    DescriptionField,
+    description,
+    TitleField,
+    title,
+    properties,
+    required,
+    uiSchema,
+    idSchema,
+  } = props;
+
   const classes = useStyles();
 
   return (
-    <>
-      {(uiSchema['ui:title'] || title) && (
-        <TitleField
-          id={`${idSchema.$id}-title`}
-          title={title}
-          required={required}
-        />
-      )}
-      {description && (
-        <DescriptionField
-          id={`${idSchema.$id}-description`}
-          description={description}
-        />
-      )}
-      <Grid container={true} spacing={2} className={classes.root}>
-        {properties.map((element: any, index: number) => (
-          <Grid
-            item={true}
-            xs={12}
-            key={index}
-            style={{ marginBottom: '10px' }}
-          >
-            {element.content}
-          </Grid>
-        ))}
-      </Grid>
-    </>
+    <ObjectFieldTemplateContext.Consumer>
+      {templates => {
+        const objectFieldTemplate = uiSchema['ui:template'];
+        if (
+          objectFieldTemplate &&
+          templates.hasOwnProperty(objectFieldTemplate)
+        ) {
+          const ObjectFieldTemplate = templates[objectFieldTemplate];
+          return <ObjectFieldTemplate {...props} />;
+        }
+
+        const uiOptions = getUiOptions(uiSchema);
+
+        let { label: displayLabel = true } = uiOptions;
+
+        return (
+          <>
+            {displayLabel && (uiSchema['ui:title'] || title) && (
+              <TitleField
+                id={`${idSchema.$id}-title`}
+                title={title}
+                required={required}
+              />
+            )}
+            {description && (
+              <DescriptionField
+                id={`${idSchema.$id}-description`}
+                description={description}
+              />
+            )}
+            <Grid container={true} spacing={2} className={classes.root}>
+              {properties.map((element: any, index: number) => (
+                <Grid
+                  item={true}
+                  xs={12}
+                  key={index}
+                  style={{ marginBottom: '10px' }}
+                >
+                  {element.content}
+                </Grid>
+              ))}
+            </Grid>
+          </>
+        );
+      }}
+    </ObjectFieldTemplateContext.Consumer>
   );
 };
 
